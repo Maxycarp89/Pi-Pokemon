36feb045fcd1008eef6dbdfcd9e1b4bc07c0285b
Maxycarp89 <maxicostilla21@gmail.com>
2022-10-11 11:58:32 -0300

    base de datos y modelo final

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,46 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+
     },
-  });
+    hp: {
+      type: DataTypes.STRING,
+    },
+    attack: {
+      type: DataTypes.STRING,
+    },
+    defense: {
+      type: DataTypes.STRING,
+    },
+    speed: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING,
+    },
+    img: {
+      type: DataTypes.STRING,
+    },
+    createdInBd: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  }, {
+    createdAt: false,
+    updatedAt: false,
+  })
 };
+
