7016a70b988474b809ad2cef3af9cc79ed1cdcaa
Maxycarp89 <maxicostilla21@gmail.com>
2022-10-19 13:10:25 -0300

    app

---
+++ client/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+
+
+
+export const getPokemons =()=>{
+    return async (dispatch) =>{
+        try {
+            let url = 'http:localhost:3001/pokemons';
+            let json = await axios.get(url);
+            return dispatch({
+                type:GET_POKEMONS,
+                payload:json.data
+        });
+        }catch(e){
+            console.log(e);
+        };
+    };
+};
+
