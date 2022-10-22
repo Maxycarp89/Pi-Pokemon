e25ebced0473aae4e1c656c46e843e4aea7386cd
Maxycarp89 <maxicostilla21@gmail.com>
2022-10-21 22:43:06 -0300

    creando card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,45 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import noImage from "../../img/noImage.png";
 
+function Card(name, image, types, id) {
+  return (
+    <div>
+      <NavLink to={`/pokemon/${id}`}>
+        <div>
+          <img
+            src={image ? image : noImage}
+            alt="img not found"
+            width="250px"
+            height="250vh"
+          />
+          <h2>{name.charAt(0).toUpperCase() + name.slice(1)} </h2>
+          <div>
+            {types?.map((e, k) => {
+              return (
+                <div key={k}>
+                  <img src={e.img} alt="X" />
+                  <p>{e.name.charAt(0).toUpperCase() + e.name.slice(1)}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </NavLink>
+    </div>
+  );
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+export default Card;
